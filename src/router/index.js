3676e9633292040f817cb79d21d1b9224ad459bb
Chakshing Lam <chakshinglam@gmail.com>
2023-01-28 20:33:38 -0500

    成功配置路由规则

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,29 @@
 import { createRouter, createWebHashHistory} from 'vue-router'
 
+import works from '../components/main_pages/works.vue'
+import playground from '../components/main_pages/playground.vue'
+import about from '../components/main_pages/about.vue'
 
 
 
 export default  createRouter({
     history: createWebHashHistory(),
     routes:[
-
+      {   
+        path:'/',
+        redirect:'/works',
+      },{   
+          name:'works',
+          path:'/works',
+          component: works,
+      },{   
+        name:'playground',
+        path:'/playground',
+        component: playground,
+      },{   
+        name:'about',
+        path:'/about',
+        component: about,
+      }
     ],    
   }) 
