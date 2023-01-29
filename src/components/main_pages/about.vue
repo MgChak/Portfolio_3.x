8685ffcd13e3b4c319e6a3f88807046c39eb25b2
Chakshing Lam <chakshinglam@gmail.com>
2023-01-28 20:28:23 -0500

    创建空页面用于配置路由规则

---
+++ src/components/main_pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+
+    <h1>about</h1>
+    
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h1{
+    color:white;
+}
+
+</style>
---
+++ src/components/main_pages/works.vue
@@ -0,0 +1,16 @@
+<template>
+
+    <h1>works</h1>
+    
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h1{
+    color:white;
+}
+
+</style>
